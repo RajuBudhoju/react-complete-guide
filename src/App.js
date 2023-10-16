69db3c5f1e6a41a51f52ff3cb991b329401f0fc2
RajuBudhoju <budhojuraju>
2023-10-16 14:32:37 +0530

    sub component folders created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import logo from './logo.svg';
-import ExpenseItem from './components/ExpenseItem';
+import ExpenseItem from './components/Expenses/ExpenseItem';
+import Expenses from './components/Expenses/Expenses';
 import './App.css';
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: 'e1',
@@ -33,12 +34,12 @@ function App() {
   return (
     <div>
       <h2>Let's get started</h2>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} ></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} ></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} ></ExpenseItem>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} ></ExpenseItem>
+      <Expenses items={expenses}/>
     </div>
   );
 }
-
+ 
 export default App;
+
+
+
